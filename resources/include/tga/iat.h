878b6577d4d93fc692cdd4291fe6aa7be28d8965
William Tremblay <tremwil@gmail.com>
2023-08-17 00:17:14 -0400

    fix IAT address lookup code

--- resources/include/tga/iat.h
+++ resources/include/tga/iat.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include <string.h>
 
-void** resolve_iat_import_ptr(HMODULE tgt_module, const char* tgt_library_name, const char* function_name) {
+void** resolve_iat_ptr(HMODULE tgt_module, const char* tgt_library_name, const char* function_name) {
     uintptr_t module = (uintptr_t)tgt_module;
     PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)module;
     PIMAGE_NT_HEADERS nt_headers = (PIMAGE_NT_HEADERS)(module + dos_header->e_lfanew);
@@ -13,12 +13,10 @@ void** resolve_iat_import_ptr(HMODULE tgt_module, const char* tgt_library_name, 
     IMAGE_DATA_DIRECTORY imports_directory = nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
     PIMAGE_IMPORT_DESCRIPTOR import_descriptor = (PIMAGE_IMPORT_DESCRIPTOR)(module + imports_directory.VirtualAddress);
 
-    while (import_descriptor->Name != 0) {
+    for (; import_descriptor->Name != 0; import_descriptor++) {
         const char* library_name = (const char*)(module + import_descriptor->Name);
-        if (stricmp(library_name, tgt_library_name)) {
-            import_descriptor++;
+        if (stricmp(library_name, tgt_library_name))
             continue;
-        }
 
         PIMAGE_THUNK_DATA original_first_thunk = (PIMAGE_THUNK_DATA)(module + import_descriptor->OriginalFirstThunk);
         PIMAGE_THUNK_DATA first_thunk = (PIMAGE_THUNK_DATA)(module + import_descriptor->FirstThunk);
@@ -28,8 +26,6 @@ void** resolve_iat_import_ptr(HMODULE tgt_module, const char* tgt_library_name, 
                 continue;
 
             PIMAGE_IMPORT_BY_NAME import_by_name = (PIMAGE_IMPORT_BY_NAME)(module + original_first_thunk->u1.AddressOfData);
-
-            //CELUA_ExecuteFunctionAsync("asyncPrint(readString(parameter, 256, false))", (size_t)import_by_name->Name);
             if (strcmp(import_by_name->Name, function_name)) 
                 continue;
 
